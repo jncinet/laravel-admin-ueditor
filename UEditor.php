d8e76056661b3849bfd39bf4d57073bcac607800
jncinet <130566114@qq.com>
2018-01-20 22:55:59 +0800

    Add files via upload

---
+++ UEditor.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Admin\Extensions\Form;
+
+use Encore\Admin\Form\Field;
+
+class UEditor extends Field
+{
+    public static $js = [
+        '/vendor/ueditor1_4_3_3-utf8/ueditor.config.js',
+        '/vendor/ueditor1_4_3_3-utf8/ueditor.all.min.js',
+        '/vendor/ueditor1_4_3_3-utf8/lang/zh-cn/zh-cn.js',
+    ];
+
+    protected $view = 'admin.ueditor';
+
+    public function render()
+    {
+        $this->script = <<<EOT
+        UE.delEditor('$this->id');
+        var ue_$this->id = UE.getEditor('$this->id');
+        ue_$this->id.ready(function () {
+            ue.execCommand('serverparam', '_token', '{{ csrf_token() }}');
+        });
+EOT;
+        return parent::render();
+    }
+}
---
+++ ueditor.blade.php
@@ -0,0 +1,8 @@
+<div class="form-group {!! !$errors->has($errorKey) ?: 'has-error' !!}">
+    <label for="{{$id}}" class="col-sm-2 control-label">{{$label}}</label>
+    <div class="col-sm-8">
+        @include('admin::form.error')
+        <script id="{{$id}}" type="text/plain" name="{{$name}}" style="width: 100%;height: 500px;">{{ old($column, $value) }}</script>
+        @include('admin::form.help-block')
+    </div>
+</div>
