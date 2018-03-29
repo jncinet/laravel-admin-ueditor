e5d6aced8ef352181a2bc51ce95d7876a4137718
张晔 <130566114@qq.com>
2018-03-30 01:03:47 +0800

    Update ueditor.blade.php

--- ueditor.blade.php
+++ ueditor.blade.php
@@ -2,7 +2,7 @@
     <label for="{{$id}}" class="col-sm-2 control-label">{{$label}}</label>
     <div class="col-sm-8">
         @include('admin::form.error')
-        <script id="{{$id}}" type="text/plain" name="{{$name}}" style="width: 100%;height: 500px;">{{ old($column, $value) }}</script>
+        <script id="{{$id}}" type="text/plain" name="{{$name}}" style="width: 100%;height: 500px;">{!! htmlspecialchars_decode(old($column, $value)) !!}</script>
         @include('admin::form.help-block')
     </div>
 </div>
